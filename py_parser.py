df3fca3c6f0f3730840bca5297da4eccb57c57bc
ranjabi <fikriranjabi02@gmail.com>
2021-11-21 01:43:29 +0700

    add python parser

---
+++ py_parser.py
@@ -0,0 +1,29 @@
+import os
+import re 
+
+# cwd = os.getcwd()
+
+def parser(filename):
+    with open(filename, encoding="utf8") as f:
+        stripped = f.read().split() # Split input di spasi ke dalam list
+        
+        # Operator yang didekomposisi
+        operator = ['+','-','*','/','=','>=','<=','==','!=','%','**','//','(',')',"'",'"',':','.',',']
+        # Mengembalikan operator dengan tambahan spasi. ex: '+' menjadi ' + '
+        operatorReplace = [' {} '.format(elem) for elem in operator]
+        
+        result = stripped
+        # Mengganti operator dengan operator replace
+        for content,contentReplace in zip(operator,operatorReplace):
+                result = [string.replace(content,contentReplace) for string in result]
+                
+        # Split lagi spasi pada list
+        result = [word for token in result for word in token.split()]
+            
+        # print(result)
+        # print(len(result))
+    return result
+
+# Usage    
+print(parser("test1.txt"))
+
