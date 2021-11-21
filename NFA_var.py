5a48594689147dc4033a581b8fb716c96e156145
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2021-11-21 13:21:42 +0700

    Update NFA_var.py

--- NFA_var.py
+++ NFA_var.py
@@ -1,28 +1,38 @@
+# Initial state
 def stateP(string):
-    # 
+    # alphabet that allows transition from P to Q
     PtoQ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_'
-    #
+    # check if string is empty
     if(len(string)==0):
         return False
-          
+    # process first input      
     else: 
         if (string[0] in PtoQ):
+            # input is True, recursively check rest of strings
             return stateQ(string[1:]) 
         else:
             return False
-         
+
+# Final state         
 def stateQ(string):
+    # alphabet that allows transition from Q to Q
     QtoQ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_0123456789'
+    # check if string is empty
     if(len(string)==0):
         return True
-          
+    # process first input
     else:  
         if (string[0] in QtoQ):
+            # input is True, recursively check rest of strings
             return stateQ(string[1:])
         else:
             return False
 
+# NFA
+def accepts(string, initial):
+    return initial(string)
+
 #take input
 var = input()
   
-print(stateP(var))
+print(accepts(var, stateP))
