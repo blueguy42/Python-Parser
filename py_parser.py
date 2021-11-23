64a1dfd804be2644a40d9b8239f19fa95428d9a9
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2021-11-24 02:36:00 +0700

    tryin to fix quotation mark issues

--- py_parser.py
+++ py_parser.py
@@ -1,6 +1,41 @@
 import os
 import re 
 
+# Initial state
+def stateP(string):
+    # alphabet that allows transition from P to Q
+    PtoQ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_'
+    # check if string is empty
+    if(len(string)==0):
+        return False
+    # process first input      
+    else: 
+        if (string[0] in PtoQ):
+            # input is True, recursively check rest of strings
+            return stateQ(string[1:]) 
+        else:
+            return False
+
+# Final state         
+def stateQ(string):
+    # alphabet that allows transition from Q to Q
+    QtoQ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_0123456789'
+    # check if string is empty
+    if(len(string)==0):
+        return True
+    # process first input
+    else:  
+        if (string[0] in QtoQ):
+            # input is True, recursively check rest of strings
+            return stateQ(string[1:])
+        else:
+            return False
+
+# NFA
+def accepts(string, initial):
+    return initial(string)
+
+
 # cwd = os.getcwd()
 
 terminals = ['input','print','exit','#','type','global','str','int','float','complex','list','tuple','range','dict','set','bool','bytes',"'",'"',"'''",'"""','(',')','[',']','{','}','+','-','*','/','%','//','**','=','+=','-=','*=','/=','%=','//=','**=','&','^','~','<<','>>','&=','^=','>>=','<<=','==','!=','>','<','>=','<=','and','or','not','is','in','\"','\'','\\','\n','\r','True','False','None','for','while','if','else','elif','pass','break','continue','def','return','class','import','dir','from','as','open','read','readline','close','write',':','.','_',',',';']
@@ -16,7 +51,6 @@ def is_number(string):
 def parser(filename):
     with open(filename, encoding="utf8") as f:
         stripped = f.read().split() # Split input di spasi ke dalam list
-
         a = []
 
         comment = False
@@ -26,14 +60,18 @@ def parser(filename):
                 comment = True
             elif i in ["'''",'"""','"',"'"] and comment:
                 comment = False
-                a.append('COMMENT')
+                a.append('comment')
 
             if not comment:
                     a.append(i)
         stripped = a
-                
+
+        
+        comparison = ['>','>=','<','<=','==','!=']
+        assignment = ['=','+=','-=','*=','+=','-=','*=','/=','%=','//=','**=']
+        other_ops = ['(',')',"'",'"',':','.',',','%','**','//','+','-','*','/']
         # Operator yang didekomposisi
-        operator = ['+','-','*','/','=','>=','<=','==','!=','%','**','//','(',')',"'",'"',':','.',',']
+        operator = ['>','>=','<','<=','==','!=','+=','-=','*=','+=','-=','*=','/=','%=','//=','**=','%','**','//','+','-','*','/','=','(',')',"'",'"',':','.',',']
         # Mengembalikan operator dengan tambahan spasi. ex: '+' menjadi ' + '
         operatorReplace = [' {} '.format(elem) for elem in operator]
         
@@ -41,20 +79,21 @@ def parser(filename):
         # Mengganti operator dengan operator replace
         for content,contentReplace in zip(operator,operatorReplace):
                 result = [string.replace(content,contentReplace) for string in result]
-                
+        
         # Split lagi spasi pada list
         result = [word for token in result for word in token.split()]
 
-        print(result)
-
         a = []
         for i in result:
-            if i in terminals or i=="COMMENT":
+            if i in terminals or i=="comment":
                 a.append(i)
             elif is_number(i):
-                a.append("INTEGER")
+                a.append("number")
             else:
-                a.append("VAR")
+                if accepts(i,stateP):
+                    a.append("var")
+                else:
+                    return "Invalid variable name!"
         result = a
             
     return result
@@ -89,8 +128,11 @@ def parser_line(filename):
     return result
 
 # Usage    
-a = parser("test1.txt") # ini one line
+a = parser("test2.txt") # ini one line
 print(a)
 # print("\n")
 # print(parser_line("test1.txt")) # ini per line, tapi aturan def sama do_something gaperlu beda bracket lagi, jadinya satu bracket untuk 1 line aja
 
+# KALO MAU DILANJUTIN KE CYK
+if a != "Invalid variable name!":
+    pass
