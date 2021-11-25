985c6db91fbaf6c2e54122c81fda636a268ffeb0
LordGedelicious <gede.coolguy@gmail.com>
2021-11-25 23:09:54 +0700

    Versi baru dari cnf sama grammar
    Note : dikerjain stlh deadline tbfo (periode demo)

--- test1.py
+++ test1.py
@@ -1,13 +1,1 @@
-def do_something(x):
-    # assalamualaikum is is
-    if x == 0:
-        return 0
-    elif x + 4 == 1:
-        if True:
-            return 3
-        else:
-            return 2
-    elif x == 32:
-        return 4
-    else:
-        return "Doodoo"
+x = func2(2,3)
