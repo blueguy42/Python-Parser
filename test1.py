ff84a2a4934d01dda02fabb1c1faeb513a6ef161
LordGedelicious <gede.coolguy@gmail.com>
2021-11-25 23:52:15 +0700

    Versi selesai dengan sedikit editan di testcasenya

--- test1.py
+++ test1.py
@@ -1,1 +1,28 @@
-x = func2(2,3)
+import package
+from package1 import function
+from package1 import function1, function2
+
+# This a sample singleline
+
+def function4(param1,param2):
+    for i in range(param1):
+        print(param2)
+        if i == param1:
+            break
+        else:
+            continue
+
+# This
+# A
+# Multiline
+
+x = "ABCDEFGJIJ" + 1 + None
+
+if not False: 
+    function4(3,x)
+elif True: 
+    print("a")
+else: 
+    print("b")
+
+# End of program
--- test2.py
+++ test2.py
@@ -1,13 +1,36 @@
-def do_something(x):
-    ''' This a sample multiline comment
-    '''
-    x + 2 = 3
-    if x == 0 + 1
-        return 0
-    elif x + 4 == 1:
-        else:
-            return 2
-    elif x == 32:
-        return 4
-    else:
-        return "Doodoo"
+import pandas as pd
+from copy import copy
+
+class Pandaren:
+    
+    def __init__(self, csvfile):
+        self.df = pd.read_csv(csvfile)
+    
+    def show(self):
+        print(self.df)
+    
+    def drop(self,col):
+        self.df.drop(columns=col)
+    
+    def getDF(self):
+        return self.df
+    
+    def getDuplicateDF(self):
+        return copy(self.df)
+    
+
+instance = Pandaren("namafilecsv","paraminiharusnyagadatapigapapakarenabukansyntaxerror")
+
+instance2 = instance.getDF()
+
+instance3 = instance.getDuplicateDF()
+
+illegal = ["col1", "col2", "col3"]
+for illegal_col in illegal:
+    instance.drop(illegal_col)
+
+if instance2.columns == instance.columns:
+    print("See i told you it would refer to the same df")
+
+if instance3.columns != instance.columns:
+    print("See the one using copy didnt get changed the columns still all there")
